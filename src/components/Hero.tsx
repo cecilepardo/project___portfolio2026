c28eb6782d344efdd638d29b9da0d1ed2b83d768
cecilepardo <pardo.cle@gmail.com>
2026-01-24 21:43:22 +0100

    GitHub and LinkedIn links, double hero light and dark

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,27 +1,27 @@
-import heroImage from "../assets/hero.jpg";
-import "./Hero.css"; // N'oublie pas d'importer le CSS si ce n'est pas déjà fait
+import "./Hero.css";
 
 const Hero = () => {
-	return (
-		<section
-			className="hero-container"
-			style={{ 
-    backgroundImage: `linear-gradient(to bottom, transparent 60%, var(--color-space) 100%), url(${heroImage})` 
-  }}
-		>
-			<div className="hero-overlay">
-				<div className="hero-content">
-					<h1 className="hero-title">Cécile Pardo</h1>
-					<h2 className="hero-subtitle">Développeuse Fullstack</h2>
-					<p className="hero-description">
-						En recherche d’une alternance full-stack, je mets mon œil de
-						designer et ma rigueur de développeuse au service d’expériences
-						numériques centrées utilisateur.
-					</p>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section
+      className="hero-container"
+      style={{ 
+        /* 2. Remplace url(${heroImage}) par var(--hero-bg-image) */
+        backgroundImage: `linear-gradient(to bottom, transparent 60%, var(--color-space) 100%), var(--hero-bg-image)` 
+      }}
+    >
+      <div className="hero-overlay">
+        <div className="hero-content">
+          <h1 className="hero-title">Cécile Pardo</h1>
+          <h2 className="hero-subtitle">Développeuse Fullstack</h2>
+          <p className="hero-description">
+            En recherche d’une alternance full-stack, je mets mon œil de
+            designer et ma rigueur de développeuse au service d’expériences
+            numériques centrées utilisateur.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
